c711e13b0fdfa84af55598d40354acf6f7cdc894
Markus Lauff <github@markus.lauff.com>
2022-01-24 17:45:26 +0100

    fixing release requirements

--- admin/words.js
+++ admin/words.js
@@ -2,28 +2,52 @@
 'use strict';
 
 systemDictionary = {
-	'rct adapter settings': {
-		'en': 'Adapter settings for rct',
-		'de': 'Adaptereinstellungen für rct',
-		'ru': 'Настройки адаптера для rct',
-		'pt': 'Configurações do adaptador para rct',
-		'nl': 'Adapterinstellingen voor rct',
-		'fr': "Paramètres d'adaptateur pour rct",
-		'it': "Impostazioni dell'adattatore per rct",
-		'es': 'Ajustes del adaptador para rct',
-		'pl': 'Ustawienia adaptera dla rct',
-		'zh-cn': 'rct的适配器设置'
+	'RCT adapter settings': {
+		'en': 'Adapter settings for RCT',
+		'de': 'Adaptereinstellungen für RCT',
+		'ru': 'Настройки адаптера для RCT',
+		'pt': 'Configurações do adaptador para RCT',
+		'nl': 'Adapterinstellingen voor RCT',
+		'fr': "Paramètres de l'adaptateur pour RCT",
+		'it': "Impostazioni dell'adattatore per RCT",
+		'es': 'Configuración del adaptador para RCT',
+		'pl': 'Ustawienia adaptera dla RCT',
+		'zh-cn': 'RCT 的适配器设置'
 	},
-	'rct_ip': {
+	'RCT_ip': {
 		'en': 'RCT IP-Address',
 		'de': 'RCT IP-Adresse',
+		'ru': 'IP-адрес RCT',
+		'pt': 'Endereço IP RCT',
+		'nl': 'RCT IP-adres',s
+		'fr': 'Adresse IP RCT',
+		'it': 'Indirizzo IP RCT',
+		'es': 'Dirección IP RCT',
+		'pl': 'Adres IP RCT',
+		'zh-cn': 'RCT IP地址'
 	},
-	'rct_refresh': {
+	'RCT_refresh': {
 		'en': 'Refresh Interval (seconds)',
-		'de': 'Aktualisierung (Sekunden)',
+		'de': 'Aktualisierungsintervall (Sekunden)',
+		'ru': 'Интервал обновления (секунды)',
+		'pt': 'Intervalo de atualização (segundos)',
+		'nl': 'Vernieuwingsinterval (seconden)',
+		'fr': 'Intervalle de rafraîchissement (secondes)',
+		'it': 'Intervallo di aggiornamento (secondi)',
+		'es': 'Intervalo de actualización (segundos)',
+		'pl': 'Interwał odświeżania (sekundy)',
+		'zh-cn': '刷新间隔（秒）'
 	},
-	'rct_elements': {
+	'RCT_elements': {
 		'en': 'RCT Elements',
-		'de': 'RCT Elemente',
+		'de': 'RCT-Elemente',
+		'ru': 'Элементы RCT',
+		'pt': 'Elementos RCT',
+		'nl': 'RCT-elementen',
+		'fr': 'Éléments RCT',
+		'it': 'Elementi RCT',
+		'es': 'Elementos de ECA',
+		'pl': 'Elementy RCT',
+		'zh-cn': '随机对照试验要素'
 	}
 };
