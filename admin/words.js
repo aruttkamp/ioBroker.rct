e24326cb7737435e8bf993958e354d59b5bcda97
Ingo Fischer <github@fischer-ka.de>
2022-09-18 21:38:10 +0200

    *add missing default values for config settings

--- admin/words.js
+++ admin/words.js
@@ -14,7 +14,7 @@ systemDictionary = {
 		'pl': 'Ustawienia adaptera dla RCT',
 		'zh-cn': 'RCT 的适配器设置'
 	},
-	'RCT_ip': {
+	'rct_ip': {
 		'en': 'RCT IP-Address',
 		'de': 'RCT IP-Adresse',
 		'ru': 'IP-адрес RCT',
@@ -26,7 +26,7 @@ systemDictionary = {
 		'pl': 'Adres IP RCT',
 		'zh-cn': 'RCT IP地址'
 	},
-	'RCT_refresh': {
+	'rct_refresh': {
 		'en': 'Refresh Interval (seconds)',
 		'de': 'Aktualisierungsintervall (Sekunden)',
 		'ru': 'Интервал обновления (секунды)',
@@ -38,7 +38,7 @@ systemDictionary = {
 		'pl': 'Interwał odświeżania (sekundy)',
 		'zh-cn': '刷新间隔（秒）'
 	},
-	'RCT_elements': {
+	'rct_elements': {
 		'en': 'RCT Elements',
 		'de': 'RCT-Elemente',
 		'ru': 'Элементы RCT',
@@ -49,5 +49,17 @@ systemDictionary = {
 		'es': 'Elementos de ECA',
 		'pl': 'Elementy RCT',
 		'zh-cn': '随机对照试验要素'
+	},
+	'rct_elements_desc': {
+		"en": "Enter a comma separated list of fields you want to query. Enter nothing to use a default field list (see README of the adapter).",
+		"de": "Geben Sie eine komma getrennte Liste von Feldern ein, die Sie abfragen möchten. Geben Sie nichts ein, um eine Standardfeldliste zu verwenden (siehe README des Adapters).",
+		"ru": "Введите комму отдельный список полей, которые вы хотите запросить. Введите ничего, чтобы использовать список по умолчанию (см. README адаптера).",
+		"pt": "Digite uma lista separada por vírgula de campos que deseja consultar. Não insira nada para usar uma lista de campo padrão (veja README do adaptador).",
+		"nl": "Voer een verdeelde lijst van velden in die je wilt keuren. Enter niets om een default veldlijst te gebruiken.",
+		"fr": "Entrez une liste séparée des champs que vous voulez demander. N'entrez rien pour utiliser une liste de champ par défaut (voir README de l'adaptateur).",
+		"it": "Inserisci un elenco separato da virgola di campi che si desidera query. Non inserire nulla per utilizzare un elenco di campo predefinito (vedere README dell'adattatore).",
+		"es": "Introduzca una lista separada de los campos que desea consultar. No introduzcas nada para usar una lista de campo predeterminada (ver README del adaptador).",
+		"pl": "Wejście do koma podzielonej listy pól, które chcesz zapytać. Wejście nie ma domyślnej listy terenowej (zobacz README of the adapter).",
+		"zh-cn": "你们想问一块离乡的区名单。 只使用缺勤外地清单(见适应人员)。."
 	}
 };
