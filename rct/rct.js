fc723aefe539f89d07b553de8ba91ead8d3654fe
Matthias Petry <127289023+NCIceWolf@users.noreply.github.com>
2024-05-22 09:15:57 +0200

    Update rct.js

--- rct/rct.js
+++ rct/rct.js
@@ -80,12 +80,14 @@ rct.end = function (host, iobInstance) {
 
 rct.process = function (host, rctElements, iobInstance) {
 
-	if (__client.destroyed==false) {
+	if (__client) {
+		if (__client.destroyed==false) {
 		try {
 			__client.destroy();
 			iobInstance.log.error('RCT: connection error! Previous stream not closed!');
 		} catch (err) {
 			iobInstance.log.error('RCT: connection error! Previous stream not closed and closure failed!');
+		}
 		}
 	}
 	
