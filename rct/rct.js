b0e2bb4321acf8b0766ed65fe5483ef07e9b5b76
Matthias Petry <127289023+NCIceWolf@users.noreply.github.com>
2024-07-02 09:06:36 +0200

    Update rct.js
    Enabled debug logging

--- rct/rct.js
+++ rct/rct.js
@@ -6,7 +6,7 @@ const rct = require('./rct_core2.js');
 module.exports = rct;
 
 // flag for local debugging
-const DEBUG_CONSOLE = false;
+const DEBUG_CONSOLE = true;
 let __refreshTimeout = null;
 let __reconnect = null;
 let __client = null;
