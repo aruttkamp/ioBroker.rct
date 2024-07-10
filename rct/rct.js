e11591485b5eeb55f8f2b6de4b918dcc14a2d7f8
Matthias Petry <127289023+NCIceWolf@users.noreply.github.com>
2024-07-10 08:03:15 +0200

    Update rct.js

--- rct/rct.js
+++ rct/rct.js
@@ -144,7 +144,7 @@ rct.process = function (host, rctElements, iobInstance) {
 					if (!__client) {
 						return;
 					}
-					if (DEBUG_CONSOLE) iobInstance.log.debug(`RCT: Element "` + rctElements + `" received from inverter`);
+					if (DEBUG_CONSOLE) iobInstance.log.debug(`RCT: Element "` + rctElements.stateFullName + `" received from inverter`);
 				});
 			}
 			requestElements();
